72c45ce36b969189900dd25631d52f9298f91c99
Diego1865 <dibaos1b@gmail.com>
2025-04-29 10:55:30 -0600

    Actualización a código, pirámide 3D

--- app/src/main/java/com/example/proyecto3b/MainActivity.java
+++ app/src/main/java/com/example/proyecto3b/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.proyecto3b;
 
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
-
+    private VideoView videoView;
+    private Button playButton;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        videoView = findViewById(R.id.videoView);
+        playButton = findViewById(R.id.playButton);
+        String videoPath = "android.resource://" + getPackageName() + "/" +
+                R.raw.mivideo;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (videoView.isPlaying()) {
+                    videoView.pause();
+                    playButton.setText("Reproducir");
+                } else {
+                    videoView.start();
+                    playButton.setText("Pausar");
+                }
+            }
         });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (videoView != null) {
+            videoView.stopPlayback();
+        }
     }
 }
